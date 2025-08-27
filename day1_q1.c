037555c57363a69139ddd47386ee061302b1eabb
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-08-28 01:01:59 +0530

    q1

---
+++ day1_q1.c
@@ -0,0 +1,11 @@
+//Write a program to input two numbers and display their sum
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    sum = num1 + num2;
+    printf("The sum is: %d\n", sum);
+    return 0;
+}

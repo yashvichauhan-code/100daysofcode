cd8017b84267040a5c408838736db17959351f6c
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-07 19:37:53 +0530

    day 6

---
+++ evenodd.c
@@ -0,0 +1,16 @@
+//Write a program to input an integer and check whether it is even or odd using if–else.
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("enter a number: ");
+    scanf("%d", &num);
+    if (num%2==0)
+    {
+        printf("%d is even", num);
+    }
+    else{
+        printf("%d is odd", num);
+    }
+    return 0;
+}

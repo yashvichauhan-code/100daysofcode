04862c51681cb2c27c71dcf812577750dd5c290a
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-15 21:25:47 +0530

    day 8

---
+++ threenum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n1, n2, n3;
+    printf("enter three numbers:n1,n2,n3 ");
+    scanf("%d %d %d", &n1, &n2, &n3);
+    if (n1>n2 && n1>n3)
+    {
+        printf("%d is the largest number", n1);
+    }
+    else if (n2>n1 && n2>n3)
+    {
+        printf("%d is the largest number", n2);
+    }
+    else if (n3>n1 && n3>n2)
+    {
+        printf("%d is the largest number", n3);
+    }
+    return 0;
+}

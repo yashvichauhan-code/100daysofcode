8dd475af468cc6207e0c471e55e83fe3b73a875a
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-15 22:37:06 +0530

    day 10 q1

---
+++ triangle.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{ 
+    int a,b,c,output;
+    printf("enter side a of triangle:");
+    scanf("%d",&a);
+    printf("enter side b of triangle:");
+    scanf("%d",&b);
+    printf("enter side c of triangle:");
+    scanf("%d",&c);
+    if (a+b>c && b+c>a && a+c>b && a>0 && b>0 && c>0)
+    {
+        printf("triangle is valid\n");
+        output=1;
+    }
+    else
+    {
+        printf("triangle is invalid\n");
+        output=0;
+    }
+    if (a==b && b==c && output==1)
+    {
+        printf("it is an equilateral triangle\n");
+    }
+    else if (a==b || b==c || a==c && output==1)
+    {
+        printf("it is an isoceles triangle\n");
+    }
+    if (a*a + b*b==c*c || b*b + c*c==a*a || a*a + c*c== b*b && output==1)
+    {
+        printf("it is a right angled triangle\n");
+    }
+    else if (a!=b && b!=c && a!=c && output==1)
+    {
+        printf("it is a scalene triangle\n");
+    }
+    return 0;
+}

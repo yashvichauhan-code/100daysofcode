bb8bbcc7a38be38a0e03e3af7f02ef12e1a65ba8
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-23 00:37:00 +0530

    q33

---
+++ armstrong.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int num, r, sum=0, c, temp;
+    printf("enter a number:");
+    scanf(" %d",&num);
+    temp=num;
+    while (num > 0)
+    {
+        r=num%10;
+        sum=sum+(r*r*r);
+        num=num/10;
+    }
+    num=temp;
+    if (sum==num)
+    {
+        printf("%d is an armstrong number",num);
+    }
+    else
+    {
+        printf("%d is not an armstrong number",num);
+    }
+    return 0;
+}

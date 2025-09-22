f245306315232ed143e67d91916e61c9424cbdbc
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-22 23:44:05 +0530

    day 12 q1

--- library.c
+++ library.c
@@ -7,9 +7,9 @@ int main()
     if (d<=5)
         printf("fine= ₹%d", d*2);
     else if (d<=10 && d>5)
-        printf("fine= ₹%d",d*4);
+        printf("fine= ₹%d",d*4-5*2);
     else if (d<=20 && d>10)
-        printf("fine= ₹%d", d*6);
+        printf("fine= ₹%d", d*6-5*2-5*4);
     else if (d>=30)
         printf("membership cancelled");
     return 0;

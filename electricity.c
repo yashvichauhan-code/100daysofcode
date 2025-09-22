7ccc97c64d803f8f328ebdc66274da9311533e6d
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-23 00:09:27 +0530

    day 12 q2

---
+++ electricity.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int u, bill = 0;
+    printf("enter units: ");
+    scanf("%d", &u);
+    if (u <= 100)
+        printf("bill: ₹%d",u * 5);
+    else if (u <= 200)
+        printf("bill: ₹%d", 100 * 5 + (u - 100) * 7);
+    else if (u <= 300)
+        printf("bill: ₹%d",100 * 5 + 100 * 7 + (u - 200) * 10);
+    else
+        printf("bill: ₹%d",100 * 5 + 100 * 7 + 100 * 10 + (u - 300) * 12);
+    return 0;
+}
--- library.c
+++ library.c
@@ -7,10 +7,10 @@ int main()
     if (d<=5)
         printf("fine= ₹%d", d*2);
     else if (d<=10 && d>5)
-        printf("fine= ₹%d",d*4-5*2);
+        printf("fine= ₹%d",(d-5)*4+5*2);
     else if (d<=20 && d>10)
-        printf("fine= ₹%d", d*6-5*2-5*4);
+        printf("fine= ₹%d", (d-10)*6+5*2+5*4);
     else if (d>=30)
         printf("membership cancelled");
     return 0;
-}      
+}

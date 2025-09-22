13812b33a7de0ef27d3a29ebdcd6c233eb5dba32
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-23 00:33:57 +0530

    q27-28

---
+++ odd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n, sum=0;
+    printf("enter number: ");
+    scanf("%d", &n);
+    for (int i=0; i<=n; i++)
+    {
+        if (i%2!=0)
+            sum+=i;
+    }
+    printf("%d", sum);
+    return 0;
+}

4b45673f824c76ec485682f2b281ab0650118f79
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-22 23:40:59 +0530

    day 12

---
+++ library.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int d, f=0;
+    printf("enter number of days you are late: ");
+    scanf("%d", &d);
+    if (d<=5)
+        printf("fine= ₹%d", d*2);
+    else if (d<=10 && d>5)
+        printf("fine= ₹%d",d*4);
+    else if (d<=20 && d>10)
+        printf("fine= ₹%d", d*6);
+    else if (d>=30)
+        printf("membership cancelled");
+    return 0;
+}      

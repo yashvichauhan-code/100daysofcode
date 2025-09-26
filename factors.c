4a9fb0d9acbd4206db0ae04eb2ccd1e91ac7e3ca
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-26 22:51:50 +0530

    day18 q35

---
+++ factors.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("enter number: ");
+    scanf("%d", &n);
+    for (int i=1; i<=n; i++)
+    {
+        if (n%i==0)
+        {
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}

0910286254397c51e70737eb2b5a5adf7982f283
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-29 22:38:02 +0530

    day19 q38

---
+++ sumofdigits.c
@@ -0,0 +1,17 @@
+// Program to find the sum of digits of a number
+#include <stdio.h>
+int main() 
+{
+	int n, sum = 0, digit;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	int temp = n;
+	while (temp != 0) 
+    {
+		digit = temp % 10;
+		sum += digit;
+		temp /= 10;
+	}
+	printf("Sum of digits of %d is %d\n", n, sum);
+	return 0;
+}

26bd063fa2c835d552d8a13f2f47bec183b6df3d
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-29 22:34:22 +0530

    day19 q1

---
+++ lcm.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+    int a, b, x, y, temp;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    x =  a, y = b;
+    while (y != 0) 
+    {
+        temp = y;
+        y = x % y;
+        x = temp;
+    }
+    int hcf = x;
+    int lcm = (a * b) / hcf;
+    printf("LCM of %d and %d is %d\n", a, b, lcm);
+    return 0;
+}

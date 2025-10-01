fc67e483294c08779dd34328d307ea19a14487c1
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-10-01 23:41:21 +0530

    day 20 q39

---
+++ odddigi.c
@@ -0,0 +1,25 @@
+// Program to find the product of odd digits of a number
+#include <stdio.h>
+
+int main() 
+{
+	int n, product = 1, digit, has_odd = 0;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	int temp = n;
+	while (temp != 0) 
+    {
+		digit = temp % 10;
+		if (digit % 2 != 0) 
+        {
+			product *= digit;
+			has_odd = 1;
+		}
+		temp /= 10;
+	}
+	if (has_odd)
+		printf("Product of odd digits of %d is %d\n", n, product);
+	else
+		printf("No odd digits found in %d\n", n);
+	return 0;
+}

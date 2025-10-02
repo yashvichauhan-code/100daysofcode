3e66a28cd5fb140ca7ba1b380f9246a165340921
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-10-02 22:08:40 +0530

    day 28 q55

---
+++ primen.c
@@ -0,0 +1,23 @@
+// Program to print all prime numbers from 1 to n
+#include <stdio.h>
+
+int main() {
+	int n;
+	printf("Enter n: ");
+	scanf("%d", &n);
+
+	printf("Prime numbers from 1 to %d are:\n", n);
+	for (int i = 2; i <= n; i++) {
+		int is_prime = 1;
+		for (int j = 2; j * j <= i; j++) {
+			if (i % j == 0) {
+				is_prime = 0;
+				break;
+			}
+		}
+		if (is_prime)
+			printf("%d ", i);
+	}
+	printf("\n");
+	return 0;
+}

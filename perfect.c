f83a4e20b7209208eeeeba421104eb6bfb44c0d2
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-10-07 16:42:30 +0530

    day 21 q42

---
+++ perfect.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n, sum=0;
+    printf("enter number: ");
+    scanf("%d", &n);
+    for (int i=1; i<n; i++)
+    {
+        if (n%i==0)
+        {
+            sum+=i;
+        }
+    }
+    if (sum==n){
+        printf("%d is perfect", n);
+    }
+    else{
+        printf("%d is not perfect", n);
+    }
+    return 0;
+}

cfda1cf57ff0c101e583efb47883b44f23431d7c
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-10-13 21:44:40 +0530

    day 23 q46

---
+++ pattern.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int l, astr;
+    printf("enter num of lines:");
+    scanf("%d", &l);
+    printf("enter num of asterick:");
+    scanf("%d", &astr);
+    for (int i=1; i<=l; i++)
+    {
+        for(int j=1; j<=astr; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}

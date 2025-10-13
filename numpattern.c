da398d5f1ec950be5283a5580cfebddb6acd7412
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-10-13 22:05:08 +0530

    day 24 q 48

---
+++ numpattern.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int l;
+    printf("enter num of lines: ");
+    scanf("%d", &l);
+    for (int i=1; i<=l; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}

b4e01e118c37d7536e3a72758fdcd5a720dc168e
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-10-13 22:01:00 +0530

    day 24 q 47

---
+++ astrpatern.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+/*{
+    int l;
+    printf("enter num of lines: ");
+    scanf("%d", &l);
+    for (int i=1; i<=l; i++){
+        for (int j=1; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}*/
+{
+    int l;
+    printf("enter num of lines: ");
+    scanf("%d", &l);
+    for (int i=l; i>=1; i--)
+    {
+        for (int j=1; j<=i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}

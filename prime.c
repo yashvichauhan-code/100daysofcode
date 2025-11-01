476afa2d5d9d42854ff9a4f2d64d349c45849259
Yashvi Chauhan <yashvi.chauhan1909@gmail.com>
2025-11-01 23:03:31 +0530

    fix

--- prime.c
+++ prime.c
@@ -4,19 +4,17 @@ int main()
     int n;
     printf("enter number: ");
     scanf("%d", &n);
-    if (n<2)
-    {
-        printf("not prime\n");
+
+    if (n < 2) {
+        printf("%d is not prime\n", n);
+        return 0;
     }
-    for (int i=2; i<=n; i++)
-    {
-        if(n%i==0)
-        {
-            printf("not prime\n");
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            printf("%d is not prime\n", n);
+            return 0;
         }
-        break;
     }
-    printf("prime");
+    printf("%d is prime\n", n);
     return 0;
 }
-//fix

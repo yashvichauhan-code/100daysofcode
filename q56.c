8e82e165cfbad24d4d2e19441d2c295dda3fb9b2
Yashvi Chauhan <yashvi.chauhan1909@gmail.com>
2025-11-03 18:37:55 +0530

    day28

---
+++ q56.c
@@ -0,0 +1,16 @@
+//Read and print elements of a one-dimensional array.
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        printf("enter element: ");
+        scanf("%d", &arr[i]);
+    }
+    for(int i=0; i<n; i++){
+        printf(" %d ", arr[i]);
+    }
+    return 0;
+}

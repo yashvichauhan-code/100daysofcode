942a3010489f4118212b1ac16ae01738dbb58f4e
Yashvi Chauhan <yashvi.chauhan1909@gmail.com>
2025-11-04 23:50:05 +0530

    day31

---
+++ q62.c
@@ -0,0 +1,22 @@
+//Reverse an array without taking extra space.
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter no. of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("enter %d elements: ", n);
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    for(int i=0; i<n/2; i++){
+        int temp = arr[i];
+        arr[i]=arr[n-i-1];
+        arr[n-i-1]=temp;
+    }
+    printf("reversed array: ");
+    for(int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}

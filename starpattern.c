a21d1e01cf89982ada64e2ab2f67e4201e0f56e8
Yashvi Chauhan <yashvi.chauhan1909@gmail.com>
2025-11-06 20:00:16 +0530

    fix

--- starpattern.c
+++ starpattern.c
@@ -12,9 +12,9 @@ int main(){
     printf("enter number of lines: ");
     scanf("%d", &n);
     for (int i=1; i<=n; i++){
-        /*for (int j=1; j<i; j++){
+        for (int j=1; j<i; j++){
             printf(" ");
-        }*/
+        }
         for (int k=i; k<=n; k++){
                 printf("*");
             }

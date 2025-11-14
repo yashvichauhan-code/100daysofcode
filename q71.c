bcf25eeec6e3c9b5a114859c98d5f62e3b76ce4b
Yashvi Chauhan <yashvi.chauhan1909@gmail.com>
2025-11-14 21:54:20 +0530

    day36

--- q71.c
+++ q71.c
@@ -1,17 +1,22 @@
 //Read and print a matrix.
 #include<stdio.h>
 int main(){
-    int m[2][2];
+    int n, m;
+    printf("enter number of rows : ");
+    scanf("%d", &n);
+    printf("enter number of coloumb : ");
+    scanf("%d", &m);
+    int arr[n][m];
     printf("enter matrix elements: ");
-    for(int i=0; i<2; i++){
-        for(int j=0; j<2; j++){
-            scanf("%d",&m[i][j]);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            scanf("%d",&arr[i][j]);
         }
     }
     printf("matrix elements: \n");
-    for(int i=0; i<2; i++){
-        for(int j=0; j<2; j++){
-            printf("%d  ",m[i][j]);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            printf("%d  ",arr[i][j]);
         }
         printf("\n");
     }

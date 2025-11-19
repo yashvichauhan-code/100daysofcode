a41aa6bf11507e32475921d850922ed1fa27841c
Yashvi Chauhan <yashvi.chauhan1909@gmail.com>
2025-11-19 23:29:35 +0530

    day41

---
+++ q81.c
@@ -0,0 +1,15 @@
+//Count characters in a string without using built-in length functions.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+    printf("Enter a string: ");
+    scanf("%[^\n]s", str); 
+    for (int i = 0; str[i] != '\0'; i++) {
+        count++;
+    }
+    printf("Number of characters: %d\n", count);
+
+    return 0;
+}

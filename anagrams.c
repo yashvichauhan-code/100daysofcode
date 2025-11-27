111d12629f24d5e8bd7d66537814aac740ca2761
Yashvi Chauhan <yashvi.chauhan1909@gmail.com>
2025-11-27 20:50:25 +0530

    .

---
+++ anagrams.c
@@ -0,0 +1,11 @@
+//Check if two strings are anagrams of each other.
+#include <stdio.h>
+int main(){
+    char str1[20], str2[20];
+    printf("enter first string: ");
+    scanf("%[^\n]", str1);
+    printf("enter second string: ");
+    scanf("%[^\n]", str2);
+    
+    return 0;
+}
